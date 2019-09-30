9e94abbeb9545b00111d05185435d80db8b30946
jbenzshawel <jbenzshawel@gmail.com>
2019-09-30 16:13:31 -0500

    Added JavaScript client example

--- IdentityServer/AppIdentity.Web/IdentityServerConfig.cs
+++ IdentityServer/AppIdentity.Web/IdentityServerConfig.cs
@@ -51,27 +51,28 @@ namespace AppIdentity.Web
 
                     AllowOfflineAccess = true // this allows requesting refresh tokens for long lived API access
                 },
-                // TODO: Configure other clients here
                 // JavaScript Client
-                //new Client
-                //{
-                //    ClientId = "js",
-                //    ClientName = "JavaScript Client",
-                //    AllowedGrantTypes = GrantTypes.Code,
-                //    RequirePkce = true,
-                //    RequireClientSecret = false,
+                new Client
+                {
+                    ClientId = "js",
+                    ClientName = "JavaScript Client",
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RequirePkce = true,
+                    RequireClientSecret = false,
 
-                //    RedirectUris =           { "http://localhost:5003/callback.html" },
-                //    PostLogoutRedirectUris = { "http://localhost:5003/index.html" },
-                //    AllowedCorsOrigins =     { "http://localhost:5003" },
+                    RedirectUris =           { "http://localhost:5003/callback.html" },
+                    PostLogoutRedirectUris = { "http://localhost:5003/index.html" },
+                    AllowedCorsOrigins =     { "http://localhost:5003" },
 
-                //    AllowedScopes =
-                //    {
-                //        IdentityServerConstants.StandardScopes.OpenId,
-                //        IdentityServerConstants.StandardScopes.Profile,
-                //        "api1"
-                //    }
-                //}
+                    AllowedScopes =
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        "api1"
+                    }
+                }
+                // TODO: Configure other clients here
+
             };
         }
     }
